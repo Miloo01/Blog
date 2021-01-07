9633435769dec7ba794cb4e9b01d74974e7a4853
Camilo Monsalve <cmonsalsilva@gmail.com>
2021-01-07 18:01:33 -0300

    first

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,11 @@
 class PagesController < ApplicationController
+  http_basic_authenticate_with name:"desafiovamoscontodo", password:"XAHTJEAS23123%23", only: :dashboard
+
+
   def index
     @posts = Post.all.order(created_at: :desc)
+  end
+
+  def dashboard
   end
 end
